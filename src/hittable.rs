f9c57839787415e452618a2dfc071c7db6e538d1
jonathan <jonathan.rinna@gmail.com>
2023-03-20 08:25:22 +0100

    clone

--- src/hittable.rs
+++ src/hittable.rs
@@ -6,7 +6,15 @@ type Point3 = Vec3;
 pub struct HitRecord {
     pub p: Point3,
     pub normal: Vec3,
-    pub t: f64
+    pub t: f64,
+    pub front_face: bool
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vec3) {
+        self.front_face = Vec3::dot(&r.dir(), outward_normal) < 0.0;
+        self.normal = front_face ? outward_normal : -outward_normal;
+    }
 }
 
 pub trait Hittable {
